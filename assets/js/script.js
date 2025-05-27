ae5f4a753e9ca1ef112572d73c4dd012da69ba56
Ri4yy <maksimov.1913@list.ru>
2025-05-27 20:29:10 +0300

    upd 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,4 +23,41 @@ document.addEventListener('DOMContentLoaded', () => {
         resize()
     })
     resize()
+
+    // Toggler catalog
+    const dropdownToggles = document.querySelectorAll('.catalog-card__dropdown-toggle');
+    
+    dropdownToggles.forEach(toggle => {
+        toggle.addEventListener('click', (e) => {
+            e.preventDefault();
+            
+            const dropdown = toggle.closest('.catalog-card__dropdown');
+            dropdown.classList.toggle('open');
+            
+            dropdownToggles.forEach(otherToggle => {
+                if (otherToggle !== toggle) {
+                    const otherDropdown = otherToggle.closest('.catalog-card__dropdown');
+                    otherDropdown.classList.remove('open');
+                }
+            });
+        });
+    });
+
+    document.addEventListener('click', (e) => {
+        if (!e.target.closest('.catalog-card__dropdown')) {
+            const openDropdowns = document.querySelectorAll('.catalog-card__dropdown.open');
+            
+            openDropdowns.forEach(dropdown => dropdown.classList.remove('open'));
+        }
+    });
+
+    // Dropdown
+    let faqToggle = document.querySelectorAll('.faq-dropdown__toggle')
+
+    faqToggle.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            let container = e.target.closest('.faq-dropdown__item');
+            container.classList.toggle('open')
+        })
+    })
 })
--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,72 +1,121 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    spaceBetween: 20,
-    enabled: true,
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     spaceBetween: 20,
+//     enabled: true,
 
-    breakpoints: {
-        320: {
-            allowTouchMove: true,
-            slidesPerView: 1,
-            grid: {
-                rows: 1,
-                fill: "row",
-            }
-        },
-        480: {
-            allowTouchMove: true,
-            slidesPerView: 2,
-            grid: {
-                rows: 1,
-                fill: "row",
-            }
-        },
-        768: {
-            allowTouchMove: true,
-            slidesPerView: 3,
-            grid: {
-                rows: 1,
-                fill: "row",
-            }
-        },
-        1024: {
-            allowTouchMove: true,
-            slidesPerView: 2,
-            grid: {
-                rows: 2,
-                fill: "row",
-            }
-        },
-        1280: {
-            allowTouchMove: true,
-            slidesPerView: 3,
-            grid: {
-                rows: 2,
-                fill: "row",
-            }
-        },
-        1921: {
-            allowTouchMove: true,
-            slidesPerView: 4,
-            grid: {
-                rows: 2,
-                fill: "row",
-            }
-        },
-    },
+//     breakpoints: {
+//         320: {
+//             allowTouchMove: true,
+//             slidesPerView: 1,
+//             grid: {
+//                 rows: 1,
+//                 fill: "row",
+//             }
+//         },
+//         480: {
+//             allowTouchMove: true,
+//             slidesPerView: 2,
+//             grid: {
+//                 rows: 1,
+//                 fill: "row",
+//             }
+//         },
+//         768: {
+//             allowTouchMove: true,
+//             slidesPerView: 3,
+//             grid: {
+//                 rows: 1,
+//                 fill: "row",
+//             }
+//         },
+//         1024: {
+//             allowTouchMove: true,
+//             slidesPerView: 2,
+//             grid: {
+//                 rows: 2,
+//                 fill: "row",
+//             }
+//         },
+//         1280: {
+//             allowTouchMove: true,
+//             slidesPerView: 3,
+//             grid: {
+//                 rows: 2,
+//                 fill: "row",
+//             }
+//         },
+//         1921: {
+//             allowTouchMove: true,
+//             slidesPerView: 4,
+//             grid: {
+//                 rows: 2,
+//                 fill: "row",
+//             }
+//         },
+//     },
   
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
+//     // If we need pagination
+//     pagination: {
+//       el: '.swiper-pagination',
+//     },
   
-    // Navigation arrows
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+  
+//     // And if we need scrollbar
+//     scrollbar: {
+//       el: '.swiper-scrollbar',
+//     },
+// });
+
+// const swiperInfo = new Swiper('.swiper-info', {
+//     slidesPerView: 1,
+//     enabled: true,
+  
+//     // If we need pagination
+//     pagination: {
+//       el: '.swiper-pagination',
+//     },
+  
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+  
+//     // And if we need scrollbar
+//     scrollbar: {
+//       el: '.swiper-scrollbar',
+//     },
+// });
+
+const swiperInfo = new Swiper('.swiper-info', {
+    slidesPerView: 1,
+    enabled: true,
+  
+    pagination: [
+        {
+          el: '.swiper-pagination-fraction',
+          type: 'fraction',
+          formatFractionCurrent: (number) => number.toString().padStart(2, '0'),
+          formatFractionTotal: (number) => number.toString().padStart(2, '0'),
+        },
+        {
+          el: '.swiper-pagination-progressbar',
+          type: 'progressbar',
+        }
+    ],
+  
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-controls__btn--next',
+      prevEl: '.swiper-controls__btn--prev',
     },
   
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-});
+    // scrollbar: {
+    //   el: '.swiper-pagination-progressbar',
+    //   draggable: true,
+    // },
+  });
